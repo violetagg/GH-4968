a5797a721a26e7258c5b1656dcc691d20f6bb417
Violeta Georgieva <violetag@vmware.com>
2024-04-17 15:08:15 +0300

    Initial implementation

---
+++ src/test/java/org/example/MicrometerMockitoTest.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+class MicrometerMockitoTest {
+
+	@Test
+	void test() {
+		Mockito.mock(Main.class);
+	}
+}
